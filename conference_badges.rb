a7415361885da0cd7c204ff9629dd49e0e1f2ef1
Aleksandr Roslyakov <torpid@gmail.com>
2018-03-27 16:38:05 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  name_array.each do |name|
+    badge_maker(name)
+  end
+end
