404df5582b6c20da270c3e1b722e155f2e8229f6
Aleksandr Roslyakov <torpid@gmail.com>
2018-03-27 17:52:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,9 +20,8 @@ end
 
 def printer(name_array)
   final_print_array = []
-  batch_badge_creator(name_array).each do |index|
-    puts batch_badge_creator(name_array).first
-  end
+  puts batch_badge_creator(name_array).first
+
   assign_rooms(name_array).each do |index|
     puts assign_rooms(name_array).first
   end
